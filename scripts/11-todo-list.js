cef369e3c74ce745ab09aef5e904c0f3767ae5de
hiphipdora <faaay.adistiii@gmail.com>
2024-12-19 15:23:26 +0700

    menambahkan looping

--- scripts/11-todo-list.js
+++ scripts/11-todo-list.js
@@ -1,5 +1,24 @@
-const todoList = [];
+const todoList = ['make dinner', 'wash dishes'];
 
+
+renderToDoList()
+function renderToDoList(){
+
+
+
+let todoListHTML = '';
+
+for (let i = 0; i < todoList.length; i++){
+    const todo = todoList[i];
+    const html = `<p>${todo}</p>`
+    todoListHTML += html;
+}
+
+console.log(todoListHTML);
+
+document.querySelector('.js-todo-list')
+.innerHTML = todoListHTML;
+}
 function addTodo(){
     const inputElement = document.querySelector('.js-name-input');
     const name = inputElement.value;
@@ -8,4 +27,5 @@ function addTodo(){
     console.log(todoList);
 
     inputElement.value = '';
+    renderToDoList();
 }
